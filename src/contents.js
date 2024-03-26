35754f8491de415c14385479a09db73c5f843b85
Achile Egbunu <achilesolomon@gmail.com>
2024-03-25 19:36:00 -0500

    Add homePage function

---
+++ src/contents.js
@@ -0,0 +1,7 @@
+const homePageContent = () => {
+  return `Visit your local Olive Garden located at Huntsville, Alabama for a hearty Italian meal. Whether you're looking for freshly baked breadsticks or perfectly made pasta, Olive Garden has something for any appetite. 
+    We are conveniently located near University Dr. and 2 miles east of Madison Square Mall. 
+    To start dining on classic Italian recipes, visit our Italian restaurant at 3730 University Dr. NW today!`;
+};
+
+export { homePageContent };
