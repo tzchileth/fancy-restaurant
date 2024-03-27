314a5189cc8e5a30923d9723798cbfd3ad8e11f4
Achile Egbunu <achilesolomon@gmail.com>
2024-03-26 21:30:06 -0500

    Add autofocus property to buttons

--- src/index.js
+++ src/index.js
@@ -8,21 +8,28 @@ const home = document.querySelector("#home");
 const menu = document.querySelector("#menu");
 const contact = document.querySelector("#contact");
 
+homePage();
+
 document.addEventListener("click", (e) => {
   switch (e.target.id) {
     case "menu":
       content.textContent = "";
       content.style.width = "";
       createMenu();
+      menu.autofocus = true;
       break;
+
     case "contact":
       content.textContent = "";
       content.style.width = "";
       displayContacts();
+      contact.autofocus = true;
       break;
-    default:
+
+    case "home":
       content.textContent = "";
       content.style.width = "";
       homePage();
+      home.autofocus = true;
   }
 });
