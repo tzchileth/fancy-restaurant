75b032395d52acc590051ea8f2c5c4bcb085b700
Achile Egbunu <achilesolomon@gmail.com>
2024-03-26 20:41:57 -0500

    Add homepage logic

--- src/homepage.js
+++ src/homepage.js
@@ -2,13 +2,20 @@ import { homePageContent } from "./contents";
 
 const homePage = () => {
   const content = document.querySelector("#content");
+
+  const homeContainer = document.createElement("div");
+
+  homeContainer.classList.add("homeContainer");
+
   const h1 = document.createElement("h1");
   const p = document.createElement("p");
 
   h1.textContent = `Delicious Dishes`;
   p.textContent = homePageContent();
 
-  content.append(h1, p);
+  homeContainer.append(h1, p);
+
+  content.append(homeContainer);
 };
 
 export { homePage };
