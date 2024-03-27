14f55153e9f06b0d7fa98ceaac6206363ac9ed22
Achile Egbunu <achilesolomon@gmail.com>
2024-03-26 20:55:34 -0500

    Add logic for the tabs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import { homePage } from "./homepage";
+import { displayContacts } from "./contactForm";
+import { createMenu } from "./menu";
 import "./style.css";
 
-homePage();
-console.log("The beauty of perseverance");
+const content = document.querySelector("#content");
+const home = document.querySelector("#home");
+const menu = document.querySelector("#menu");
+const contact = document.querySelector("#contact");
+
+document.addEventListener("click", (e) => {
+  switch (e.target.id) {
+    case "menu":
+      content.textContent = "";
+      content.style.width = "";
+      createMenu();
+      break;
+    case "contact":
+      content.textContent = "";
+      content.style.width = "";
+      displayContacts();
+      break;
+    default:
+      content.textContent = "";
+      content.style.width = "";
+      homePage();
+  }
+});
